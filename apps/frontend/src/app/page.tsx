6c50133ee619dc38131fa98e955c0b1fd5cdb8b1
Luan Alves <97049835+Luan029@users.noreply.github.com>
2024-07-22 15:44:04 -0300

    chore: configurações de ambiente

---
+++ apps/frontend/src/app/page.tsx
@@ -0,0 +1,8 @@
+import { somar } from '@barba/core'
+export default function Home() {
+  return (
+    <div>
+      {somar(1, 2)}
+    </div>
+  );
+}
---
+++ apps/mobile/src/App.tsx
@@ -0,0 +1,8 @@
+import {Text, View} from 'react-native'
+export default function App(){
+    return(
+        <View style={{flex:1, justifyContent:'center', alignItems:'center'}}>
+            <Text>Hello, Barba-Brutal!</Text>
+        </View>
+    )
+}
