7b41e36e01213732162d01ed5b672eb834085483
Luan Alves <97049835+Luan029@users.noreply.github.com>
2024-07-23 09:37:04 -0300

    feat: adiciona serviços

---
+++ apps/frontend/src/components/services/OurServices.tsx
@@ -0,0 +1,29 @@
+import useServices from "@/data/hooks/useServices"
+import { useRouter } from "next/navigation"
+import Title from "../shared/Title"
+import { Service } from "@barba/core"
+import ItemServices from "./ItemServies"
+
+export default function OurServices() {
+    const router = useRouter()
+    const { services } = useServices()
+
+    function startAppointments() {
+        router.push('/agendamento')
+    }
+
+    return (
+        <div className="flex flex-col gap-16">
+            <Title
+                tag="Serviços"
+                primary="Do Classico ao Rock"
+                secondary="Cabelo afiado, barba de lenhador e mãos de motoqueiro, tudo ao som de rock pesado!"
+            />
+            <div className="grid grid-cols-1 lg:grid-cols-2 xl:grid-cols-3 gap-5">
+                {services.map((service: Service) => (
+                    <ItemServices key={service.id} service={service} onClick={startAppointments} />
+                ))}
+            </div>
+        </div>
+    )
+}
