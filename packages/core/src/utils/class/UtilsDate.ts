3484d43e81153b26499351f13d809681b9df0044
Luan Alves <97049835+Luan029@users.noreply.github.com>
2024-07-24 10:29:53 -0300

    feat: adiciona classes utils no core da aplicação

---
+++ packages/core/src/utils/class/UtilsDate.ts
@@ -0,0 +1,14 @@
+export default class UtiosDate {
+  static today() {
+    const today = new Date();
+    today.setHours(0, 0, 0, 0);
+    return today;
+  }
+
+  static applySchedule(date: Date, schedule: string): Date {
+    const newDate = new Date(date);
+    const parts = schedule.split(":");
+    newDate.setHours(parseInt(parts[0]!), parseInt(parts[1!]));
+    return newDate;
+  }
+}
--- packages/core/src/utils/index.ts
+++ packages/core/src/utils/index.ts
@@ -1,2 +1,4 @@
-import PhoneUtils from "./UtilsPhone";
-export {PhoneUtils}
+import PhoneUtils from "./class/UtilsPhone";
+import UtilsSchedule from "./class/UtilsSchedule";
+import UtiosDate from "./class/UtilsDate";
+export { PhoneUtils, UtilsSchedule, UtiosDate };
