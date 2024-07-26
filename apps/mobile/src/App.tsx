d24abcb035ada11736d9224ee2c70a224c5e2791
Luan Alves <97049835+Luan029@users.noreply.github.com>
2024-07-26 08:58:17 -0300

    feat: envolve a aplicação dentro dos contextos

--- apps/mobile/src/App.tsx
+++ apps/mobile/src/App.tsx
@@ -1,8 +1,17 @@
-import {Text, View} from 'react-native'
-export default function App(){
-    return(
-        <View style={{flex:1, justifyContent:'center', alignItems:'center'}}>
-            <Text>Hello, Barba-Brutal!</Text>
-        </View>
+import { Text, View } from 'react-native'
+import { createNativeStackNavigator } from '@react-navigation/native-stack'
+import { UserProvider } from './data/contexts/UserContext'
+import { AppointmentProvider } from './data/contexts/AppointmentContext'
+
+const Stack = createNativeStackNavigator()
+export default function App() {
+    return (
+        <UserProvider>
+            <AppointmentProvider>
+                <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+                    <Text>Hello, Barba-Brutal!</Text>
+                </View>
+            </AppointmentProvider>
+        </UserProvider>
     )
 }
