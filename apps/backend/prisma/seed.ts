c1006770e89416ddaff992842f4d9b4ac54acb3e
Luan Alves <97049835+Luan029@users.noreply.github.com>
2024-08-02 08:59:09 -0300

    feat: preenche banco com barbeiros

--- apps/backend/prisma/seed.ts
+++ apps/backend/prisma/seed.ts
@@ -8,6 +8,11 @@ import { professional, services, User } from '@barba/core';
 const prisma = new PrismaClient();
 
 async function seed() {
+  await prisma.appointment.deleteMany();
+  await prisma.user.deleteMany();
+  await prisma.professional.deleteMany();
+  await prisma.service.deleteMany();
+
   await prisma.professional.createMany({
     data: professional as PrismaProfessional[],
   });
@@ -21,8 +26,50 @@ async function seed() {
       phone: '11999999999',
       barber: true,
     },
+    {
+      name: 'Beto Brutal',
+      email: 'beto@barbabrutal.app',
+      password: 'Senha123#',
+      phone: '11999999999',
+      barber: true,
+    },
+    {
+      name: 'Kathya Tesourada',
+      email: 'kathya@barbabrutal.app',
+      password: 'Senha123#',
+      phone: '11999999999',
+      barber: true,
+    },
+    {
+      name: 'Sérgio Serrador',
+      email: 'sergio@barbabrutal.app',
+      password: 'Senha123#',
+      phone: '11999999999',
+      barber: true,
+    },
+    {
+      name: 'Rafa Raspa Tudo',
+      email: 'rafa@barbabrutal.app',
+      password: 'Senha123#',
+      phone: '11999999999',
+      barber: true,
+    },
+    {
+      name: 'Fernanda Faísca',
+      email: 'fernanda@barbabrutal.app',
+      password: 'Senha123#',
+      phone: '11999999999',
+      barber: true,
+    },
   ];
   await prisma.user.createMany({ data: users as any });
 }
 
-seed();
+seed()
+  .catch((e) => {
+    console.error(e);
+    process.exit(1);
+  })
+  .finally(async () => {
+    await prisma.$disconnect();
+  });
