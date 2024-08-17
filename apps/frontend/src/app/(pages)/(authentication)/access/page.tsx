62ca844943531ef00ff40184736196b63720fa3f
Luan Alves <97049835+Luan029@users.noreply.github.com>
2024-08-17 15:26:38 -0300

    refactor: corrige style do loading component

--- apps/frontend/src/app/(pages)/(authentication)/access/page.tsx
+++ apps/frontend/src/app/(pages)/(authentication)/access/page.tsx
@@ -1,6 +1,5 @@
 import LoadingComponent from "@/components/shared/LoadingComponent";
 import UserForm from "@/components/user/UserForm";
-import Image from "next/image";
 import { Suspense } from "react";
 
 export default function Page() {
--- apps/frontend/src/components/shared/LoadingComponent.tsx
+++ apps/frontend/src/components/shared/LoadingComponent.tsx
@@ -1,15 +1,20 @@
 import Image from "next/image";
+import Img from '../../../public/banners/principal.webp'
 
 export default function LoadingComponent(){
     return(
         <section className="flex justify-center items-center h-screen relative">
-        <Image
-          src="/banners/principal.webp"
-          fill
-          alt="Barbearia"
-          className="object-cover"
-        />
-        <h2 className="text-gradient text-5xl sm:text-6xl md:text-7xl lg:text-8xl font-black pb-2">Carregando...</h2>
-      </section>
+            <Image
+                src={Img}
+                fill
+                alt="Barbearia"
+                className="object-cover"
+            />
+            <div className="absolute text-center">
+                <h2 className="text-gradient text-5xl sm:text-6xl md:text-7xl lg:text-8xl font-black pb-2 z-20">
+                    Carregando...
+                </h2>
+            </div>
+        </section>
     )
 }
