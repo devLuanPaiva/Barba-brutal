5ae44395a733ee0ae0c509738ab940b1fe081c9f
Luan Alves <97049835+Luan029@users.noreply.github.com>
2024-08-19 10:36:51 -0300

    refactor: implementa autenticação nas requisições

--- apps/mobile/src/data/hooks/useAPI.ts
+++ apps/mobile/src/data/hooks/useAPI.ts
@@ -1,28 +1,57 @@
 import { useCallback } from "react";
+import useSection from "./useSection";
 
-const URL_BASE = process.env.URL_BASE;
+const URL_BASE_API = 'http://192.168.2.100:3001';
 
 export default function useAPI() {
-  const httpGET = useCallback(async function (url: string): Promise<any> {
+  const { token } = useSection();
+  const httpGET = useCallback(
+    async function (url: string): Promise<any> {
+      const path = url.startsWith("/") ? url : `/${url}`;
+      try {
+        const response = await fetch(`${URL_BASE_API}${path}`, {
+          headers: {
+            Authorization: `Bearer ${token}`,
+          },
+        });
+        return extractData(response);
+      } catch (err) {
+        console.error("Error ao executar requisição:", err);
+        throw err;
+      }
+    },
+    [token]
+  );
+
+  const httpPOST = useCallback(
+    async function (url: string, body: any): Promise<any> {
+      try {
+        const path = url.startsWith("/") ? url : `/${url}`;
+        const resp = await fetch(`${URL_BASE_API}${path}`, {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+            Authorization: `Bearer ${token}`,
+          },
+          body: JSON.stringify(body),
+        });
+        return extractData(resp);
+      } catch (err) {
+        console.error("Error ao executar requisição:", err);
+        throw err;
+      }
+    },
+    [token]
+  );
+
+  async function extractData(resp: Response) {
+    let content = "";
     try {
-      const response = await fetch(`${URL_BASE}/${url}`);
-      return await response.json();
-    } catch (err) {
-      console.error("Error ao executar requisição:", err);
-      throw err;
+      content = await resp.text();
+      return JSON.parse(content);
+    } catch (e) {
+      return content;
     }
-  }, []);
-  const httpPOST = useCallback(async function (
-    url: string,
-    body: any,
-  ): Promise<any> {
-    await fetch(`${URL_BASE}/${url}`, {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify(body),
-    });
-  }, []);
+  }
   return { httpGET, httpPOST };
 }
