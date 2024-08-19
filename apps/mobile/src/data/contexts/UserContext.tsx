ce9238e50ec55a2ac9c2c6f5280f0b92b0019e5e
Luan Alves <97049835+Luan029@users.noreply.github.com>
2024-08-19 14:22:34 -0300

    fix: remove navegação para a Home em logout

--- apps/mobile/src/data/contexts/UserContext.tsx
+++ apps/mobile/src/data/contexts/UserContext.tsx
@@ -1,22 +1,16 @@
 import React, { createContext, useCallback, useMemo } from "react";
-import { useNavigation } from "@react-navigation/native";
 import useSection from "../hooks/useSection";
 import useAPI from "../hooks/useAPI";
 import { UserContextProps } from "../interfaces";
 import { User } from "@barba/core";
-import { NativeStackNavigationProp } from "@react-navigation/native-stack";
 import Toast from 'react-native-toast-message';
 
-type RootStackParamList = {
-  Home: undefined
-}
-type NavigationProp = NativeStackNavigationProp<RootStackParamList>
+
 const UserContext = createContext<UserContextProps>({} as any);
 
 export function UserProvider({ children }: any) {
   const { httpPOST } = useAPI();
   const { clearSection, createSection, loading, user } = useSection();
-  const navigation = useNavigation<NavigationProp>();
 
   const register = useCallback(async (user: User) => {
     try {
@@ -42,8 +36,7 @@ export function UserProvider({ children }: any) {
 
   const logout = useCallback(() => {
     clearSection();
-    navigation?.navigate("Home");
-  }, [navigation, clearSection]);
+  }, [clearSection]);
 
   const contextValue = useMemo(() => {
     return {
