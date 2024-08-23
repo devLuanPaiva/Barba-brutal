89e2d0da4ecf72fec08b69f5a3dfcf57ed1e6a1c
Luan Alves <97049835+Luan029@users.noreply.github.com>
2024-08-23 16:46:25 -0300

    feat: cria bases interfaces

---
+++ apps/mobile/src/data/interfaces/BaseInputProps.interface.ts
@@ -0,0 +1,29 @@
+import { Professional, Service } from "@barba/core";
+
+export interface BaseProps<T> {
+  changedValue: (value: T) => void;
+}
+
+export interface BaseInputProps<T> extends BaseProps<T> {
+  date: Date;
+}
+
+export interface DayInputProps extends BaseInputProps<Date> {}
+
+export interface DateInputProps extends BaseInputProps<Date> {
+  amountSlots: number;
+}
+
+export interface TimesInputProps extends BaseInputProps<Date> {
+  slotsQuantity: number;
+}
+
+export interface ServicesInputProps extends BaseProps<Service[]> {
+  services: Service[];
+  onChange: (services: Service[]) => void;
+}
+
+export interface ProfessionalInputProps extends BaseProps<Professional> {
+  professionals: Professional | null;
+  onChange: (professional: Professional) => void;
+}
--- apps/mobile/src/data/interfaces/index.ts
+++ apps/mobile/src/data/interfaces/index.ts
@@ -1,2 +1,4 @@
 export * from './AppointmentContextProps.interface'
 export * from './BaseContextProps.interfece'
+export * from './BaseInputProps.interface'
+export * from './BaseItemProps.interface'
